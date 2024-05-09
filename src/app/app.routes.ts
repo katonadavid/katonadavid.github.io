c54ca6bf6e0546fcf4ebcd2b9b57ab72f566af51
David <katona.david93@gmail.com>
2024-05-09 14:24:00 +0200

    reframed app, added detail page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { CoinListComponent } from './coin-list/coin-list.component';
+import { CoinDetailsComponent } from './coin-details/coin-details.component';
 
 export const routes: Routes = [
     { path: 'list', component: CoinListComponent },
+    { path: 'coin/:coinId', component: CoinDetailsComponent },
     { path: '**', redirectTo: 'list' }
 ];
--- src/app/coin-list/coin-list.component.ts
+++ src/app/coin-list/coin-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { CoinService } from '../services/coin.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-coin-list',
@@ -14,12 +15,16 @@ export class CoinListComponent implements OnInit {
   coinListDataSource = new MatTableDataSource<any>();
   displayedColumns =['name', 'symbol'];
 
-  constructor(private coinService: CoinService) {}
+  constructor(private coinService: CoinService, private router: Router) {}
 
   ngOnInit(): void {
     this.coinService.getCoinList().subscribe(coins => {
       //TODO: Add typing and pagination
       this.coinListDataSource.data = (coins as Array<any>).slice(0, 20);
     })
+  }
+
+  openCoinDetails(coinId: string) {
+    this.router.navigate(['coin', coinId]);
   }
 }
